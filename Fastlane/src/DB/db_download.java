ba9aa67918c98f249b89af2294b4ec9379aeb58a
Hadas Shidlovski <shidlhad@post.bgu.ac.il>
2018-12-11 15:44:20 +0200

    Add DB pakage

---
+++ Fastlane/src/DB/db_download.java
@@ -0,0 +1,10 @@
+package DB;
+
+public class db_download {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Fastlane/src/DB/db_upload.java
@@ -0,0 +1,10 @@
+package DB;
+
+public class db_upload {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
