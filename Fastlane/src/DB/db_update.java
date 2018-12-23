bfd84912c5db69075609d42c9006f99a7303fb20
Hadas Shidlovski <hadashidl@gmail.com>
2018-12-23 14:40:10 +0200

    update db_update

--- Fastlane/src/DB/db_update.java
+++ Fastlane/src/DB/db_update.java
@@ -1,22 +1,53 @@
 package DB;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class db_update {
 
-	//create schema for Fastlane DB once with try & catch
-/*	public  void createSchema(Statement st) throws SQLException {
-		try {
-			 ResultSet rs = st.executeQuery("CREATE SCHEMA FastLane");
-			
-		} catch(SQLException e ){
-			 System.out.println(e.getMessage());
-		} finally {
-	        if (st != null) { st.close(); }
-	    }
+	//insert data to tables
+
+	public void insertData(Connection con) throws  FileNotFoundException, SQLException {
 		
-	}*/
+		try {
+			PreparedStatement ps = null;
+			String agency_id = "";
+			String agency_name = "";
+			String agency_url = "";
+			 BufferedReader br = new BufferedReader(new FileReader("C:/Users/hadas/Desktop/פרויקט/finalProject/finalProject/Fastlane/output/agency.txt"));
+			 String line = null;
+		        while ((line = br.readLine() )!=null)
+		        {
+		        	//Make sure the line is not null, not empty, and contains 2 comma char
+		        	if (line != null && !line.equals("") && line.matches(".*[,].*[,].*") && !line.contains("agency")) {
+		            String tmp[] = line.split(",");
+		            agency_id = tmp[0]; 
+		            agency_name = tmp[1];
+		            agency_url = tmp[2];
+		        
+		        	String query = "INSERT INTO agency (agency_id,agency_name,agency_url)"
+		        			+ "VALUES (" + Integer.parseInt(agency_id) + ",' " + agency_name + "','" + agency_url + "');" ;
+		        	ps = con.prepareStatement(query);
+		        	ps.executeUpdate();
+		        	
+		        	}
+		        }
+		        br.close();
+	        	con.close();
+		       if(ps!=null) ps.close();
+
+			
+		} catch(IOException e){
+				e.printStackTrace();
+		
+		}
+	}
 
 }
