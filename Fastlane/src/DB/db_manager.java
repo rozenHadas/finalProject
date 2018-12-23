27e1d7b9eb58ae3766e9c9e20d1caa9f17176281
Hadas Shidlovski <hadashidl@gmail.com>
2018-12-23 14:43:51 +0200

    Update db_manager

--- Fastlane/src/DB/db_manager.java
+++ Fastlane/src/DB/db_manager.java
@@ -1,12 +1,15 @@
 package DB;
 
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
 import java.util.Date;
 
 public class db_manager {
 	public static db_connect db_connect = new db_connect();
 	public static db_download download_File = new db_download();
+	public static db_update update_File = new db_update();
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException, FileNotFoundException {
 		
 		//connection to postgresDriver
 		try {
@@ -17,9 +20,9 @@ public class db_manager {
 		}
 		
 		//unzip gtfs files location
-        String zipFilePath = "C:/Users/hadas/Desktop/ôøåé÷è/FastLane/DB/raw_data/gtfs.zip";        
+        String zipFilePath = "C:/Users/hadas/Desktop/×¤×¨×•×™×§×˜/FastLane/DB/raw_data/gtfs.zip";        
         //unzip to output folder
-        String destDir = "C:/Users/hadas/Desktop/ôøåé÷è/finalProject/finalProject/Fastlane/output";
+        String destDir = "C:/Users/hadas/Desktop/×¤×¨×•×™×§×˜/finalProject/finalProject/Fastlane/output";
         //check start time for unzipping
         System.out.println("Start Time:" + new Date());
         download_File.unzip(zipFilePath, destDir);
@@ -27,8 +30,9 @@ public class db_manager {
         System.out.println("End Time:" + new Date());
         // create connection once at db_connect class;
         db_connect.createConnection();
+        //insert data files to DB
+        update_File.insertData(db_connect.getConnection());
         
-        download_File.writeToPostgress(destDir);
         System.out.println("finishhhhh");
         System.out.println("End Time postgres:" + new Date());
         
