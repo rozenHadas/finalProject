eacaf4a8b5e3e6045af1d7d8beccdb5378a8746e
shidlhad <hadashidl@gmail.com>
2018-12-28 15:41:48 +0200

    refactor for class dp_update

--- Fastlane/src/DB/db_update.java
+++ Fastlane/src/DB/db_update.java
@@ -19,6 +19,55 @@ public class db_update {
 
 	public void insertData(Connection con) throws  FileNotFoundException, SQLException {
 		
+		insertAgency(con);
+		insertRoutes(con);
+		con.close();
+	}
+
+	private void insertRoutes(Connection con) throws SQLException {
+		try {
+			PreparedStatement ps = null;
+			String route_id = "";
+			String agency_id = "";
+			String route_long_name = "";
+			String route_desc = "";
+			String route_type = "";
+			String route_color = "";
+			
+			 BufferedReader br = new BufferedReader(new InputStreamReader(
+					 new FileInputStream("C:/Users/hadas/Desktop/פרויקט/finalProject/Fastlane/output/routes.txt"), StandardCharsets.UTF_8));
+			 String line = null;
+		        while ((line = br.readLine() )!=null)
+		        {
+		        	//Make sure the line is not null, not empty, and contains 2 comma char
+		        	if (line != null && !line.equals("") && line.matches(".*[,].*[,].*") && !line.contains("route")) {
+		            String tmp[] = line.split(",");
+		            route_id = tmp[5]; 
+		            agency_id = tmp[4];
+		            route_long_name = tmp[3];
+		            route_desc = tmp[2];
+		            route_type = tmp[1]; 
+		            route_color = tmp[0];
+		        
+		        	String query = "INSERT INTO routes (route_id,agency_id,route_long_name,route_desc,route_type,route_color)"
+		        			+ "VALUES (" + Integer.parseInt(route_id) + ",'" + agency_id 
+		        			+"',' " + route_long_name + "','" + route_desc + "'," + Integer.parseInt(route_type)
+		        			+"," + Integer.parseInt(route_color) + ");" ;
+		        	ps = con.prepareStatement(query);
+		        	ps.executeUpdate();
+		        	
+		        	}
+		        }
+		        br.close();
+		       if(ps!=null) ps.close();
+			
+		} catch(IOException e){
+				e.printStackTrace();
+		
+		}
+	}
+
+	private void insertAgency(Connection con) throws SQLException {
 		try {
 			PreparedStatement ps = null;
 			String agency_id = "";
@@ -45,49 +94,6 @@ public class db_update {
 		        	}
 		        }
 		        br.close();
-	        	//con.close();
-		       if(ps!=null) ps.close();
-
-			
-		} catch(IOException e){
-				e.printStackTrace();
-		
-		}
-		
-		try {
-			PreparedStatement ps = null;
-			String route_id = "";//int
-			String agency_id = "";
-			String route_long_name = "";
-			String route_desc = "";
-			String route_type = "";//int
-			String route_color = "";//int
-			
-			 BufferedReader br = new BufferedReader(new FileReader("C:/Users/hadas/Desktop/פרויקט/finalProject/Fastlane/output/routes.txt"));
-			 String line = null;
-		        while ((line = br.readLine() )!=null)
-		        {
-		        	//Make sure the line is not null, not empty, and contains 2 comma char
-		        	if (line != null && !line.equals("") && line.matches(".*[,].*[,].*") && !line.contains("route")) {
-		            String tmp[] = line.split(",");
-		            route_id = tmp[5]; 
-		            agency_id = tmp[4];
-		            route_long_name = tmp[3];
-		            route_desc = tmp[2];
-		            route_type = tmp[1]; 
-		            route_color = tmp[0];
-		        
-		        	String query = "INSERT INTO routes (route_id,agency_id,route_long_name,route_desc,route_type,route_color)"
-		        			+ "VALUES (" + Integer.parseInt(route_id) + ",'" + agency_id 
-		        			+"',' " + route_long_name + "','" + route_desc + "'," + Integer.parseInt(route_type)
-		        			+"," + Integer.parseInt(route_color) + ");" ;
-		        	ps = con.prepareStatement(query);
-		        	ps.executeUpdate();
-		        	
-		        	}
-		        }
-		        br.close();
-	        	con.close();
 		       if(ps!=null) ps.close();
 
 			
